fa499792af65155e1629199aac320d38adbe84b5
andres alcocer <devandres.tech@gmail.com>
2022-07-26 14:12:21 -0400

    created timestamp, greetingCard, and follow models

--- ecards_api/models.py
+++ ecards_api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class TimeStamp(models.Model):
+    created_at = models.DateTimeField(db_index=True, auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class GreetingCard(TimeStamp):
+	GREEN = '#00FF00'
+	BLUE = '#0000FF'
+	RED = '#ff0000'
+	COLOR_CHOICES = [
+		(GREEN, '#00FF00'),
+		(RED, '#ff0000'),
+		(BLUE, '#0000FF'),
+	]
+	card_color_list = models.CharField(max_length=7, choices=COLOR_CHOICES, default=GREEN)
+	card_color = models.CharField(null=True, max_length=275)
+	card_owner = models.ForeignKey('auth.User', related_name='greeting_cards', on_delete=models.CASCADE)
+	card_inner_message = models.TextField(max_length=300)
+	card_outer_message = models.TextField(max_length=300)
+	card_image = models.CharField(max_length=275)
+
+class Follow(TimeStamp):
+	follower = models.ForeignKey('auth.User', on_delete=models.CASCADE)
