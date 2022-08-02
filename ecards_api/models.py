9db9fcd0a78517d67d9e82dc848b068a99250e05
andres alcocer <devandres.tech@gmail.com>
2022-08-02 18:52:13 -0400

    created migrations

--- ecards_api/models.py
+++ ecards_api/models.py
@@ -18,13 +18,13 @@ class GreetingCard(TimeStamp):
 		(RED, 'ff0000'),
 		(BLUE, '0000FF'),
 	]
-	card_color_list = models.CharField(max_length=7, choices=COLOR_CHOICES, default=GREEN)
+	card_color_list = models.CharField(max_length=200, choices=COLOR_CHOICES, default=GREEN)
 	card_color = models.CharField(null=True, max_length=200)
 	card_owner = models.ForeignKey('auth.User', related_name='greeting_cards', on_delete=models.CASCADE)
 	card_inner_message = models.TextField(max_length=300)
 	card_outer_message = models.TextField(max_length=300)
-	card_image = models.TextField(max_length=400, null=True, blank=True)
-	card_image_file = models.ImageField(upload_to='card_images', null=True, blank=True, max_length=400)
+	card_image = models.TextField(max_length=600, null=True, blank=True)
+	card_image_file = models.ImageField(upload_to='card_images', null=True, blank=True, max_length=600)
 	
 
 class Follow(TimeStamp):
