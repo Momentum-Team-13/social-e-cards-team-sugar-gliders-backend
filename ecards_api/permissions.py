8e5ceca3a68f6b8d377deadb858313e59aaca402
andres alcocer <devandres.tech@gmail.com>
2022-08-03 16:17:12 -0400

    modified permisions.py

--- ecards_api/permissions.py
+++ ecards_api/permissions.py
@@ -32,4 +32,4 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
             return True
 
         # Write permissions are only allowed to the owner of the snippet.
-        return obj.owner == request.user
+        return obj.card_owner == request.user
